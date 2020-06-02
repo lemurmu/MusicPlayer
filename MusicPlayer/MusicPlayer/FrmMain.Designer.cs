36fd1f066475d7429cd16ebf05d6ebb1fa568d8f
lemurmu <455721762@qq.com>
2020-06-02 11:15:52 +0800

    初始化提交

---
+++ MusicPlayer/MusicPlayer/FrmMain.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace MusicPlayer
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraBars.Docking2010.WindowsUIButtonImageOptions windowsUIButtonImageOptions4 = new DevExpress.XtraBars.Docking2010.WindowsUIButtonImageOptions();
+            DevExpress.XtraBars.Docking2010.WindowsUIButtonImageOptions windowsUIButtonImageOptions5 = new DevExpress.XtraBars.Docking2010.WindowsUIButtonImageOptions();
+            DevExpress.XtraBars.Docking2010.WindowsUIButtonImageOptions windowsUIButtonImageOptions6 = new DevExpress.XtraBars.Docking2010.WindowsUIButtonImageOptions();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.windowsUIButtonPanel = new DevExpress.XtraBars.Docking2010.WindowsUIButtonPanel();
+            this.mediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "Metropolis Dark";
+            // 
+            // windowsUIButtonPanel
+            // 
+            windowsUIButtonImageOptions4.Image = global::MusicPlayer.Properties.Resources.prev_32x32;
+            windowsUIButtonImageOptions5.Image = global::MusicPlayer.Properties.Resources.pause_32x32;
+            windowsUIButtonImageOptions6.Image = global::MusicPlayer.Properties.Resources.next_32x32;
+            this.windowsUIButtonPanel.Buttons.AddRange(new DevExpress.XtraEditors.ButtonPanel.IBaseButton[] {
+            new DevExpress.XtraBars.Docking2010.WindowsUIButton("Button", false, windowsUIButtonImageOptions4, DevExpress.XtraBars.Docking2010.ButtonStyle.PushButton, "上一首", -1, true, null, true, false, true, "pre", 0, false),
+            new DevExpress.XtraBars.Docking2010.WindowsUIButton("Button", false, windowsUIButtonImageOptions5, DevExpress.XtraBars.Docking2010.ButtonStyle.PushButton, "", -1, true, null, true, false, true, "pause", -1, false),
+            new DevExpress.XtraBars.Docking2010.WindowsUIButton("Button", false, windowsUIButtonImageOptions6, DevExpress.XtraBars.Docking2010.ButtonStyle.PushButton, "下一首", -1, true, null, true, false, true, "next", -1, false)});
+            this.windowsUIButtonPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.windowsUIButtonPanel.Location = new System.Drawing.Point(0, 415);
+            this.windowsUIButtonPanel.Name = "windowsUIButtonPanel";
+            this.windowsUIButtonPanel.Size = new System.Drawing.Size(721, 67);
+            this.windowsUIButtonPanel.TabIndex = 0;
+            this.windowsUIButtonPanel.Text = "windowsUIButtonPanel1";
+            this.windowsUIButtonPanel.ButtonClick += new DevExpress.XtraBars.Docking2010.ButtonEventHandler(this.windowsUIButtonPanel_ButtonClick);
+            // 
+            // mediaPlayer
+            // 
+            this.mediaPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mediaPlayer.Enabled = true;
+            this.mediaPlayer.Location = new System.Drawing.Point(0, 0);
+            this.mediaPlayer.Name = "mediaPlayer";
+            this.mediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mediaPlayer.OcxState")));
+            this.mediaPlayer.Size = new System.Drawing.Size(721, 415);
+            this.mediaPlayer.TabIndex = 1;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 482);
+            this.Controls.Add(this.mediaPlayer);
+            this.Controls.Add(this.windowsUIButtonPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My音乐播放器";
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private DevExpress.XtraBars.Docking2010.WindowsUIButtonPanel windowsUIButtonPanel;
+        private AxWMPLib.AxWindowsMediaPlayer mediaPlayer;
+    }
+}
+
---
+++ MusicPlayer/MusicPlayer/FrmMain.cs
@@ -0,0 +1,69 @@
+﻿using DevExpress.XtraBars.Docking2010;
+using MusicPlayer.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace MusicPlayer
+{
+    public partial class FrmMain : DevExpress.XtraEditors.XtraForm
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            PlayerOne();
+        }
+
+        private string fileFolder = "music";
+
+        private void PlayerOne()
+        {
+            string[] musicfiles = Directory.GetFiles(fileFolder, "*.mp3");
+            IWMPPlaylist playList = mediaPlayer.playlistCollection.newPlaylist("MyPlayList"); //新建列表
+            foreach (var musciFile in musicfiles)
+            {
+                IWMPMedia media = mediaPlayer.newMedia(musciFile);
+                playList.appendItem(media);
+            }
+
+            mediaPlayer.currentPlaylist = playList;
+            mediaPlayer.Ctlcontrols.play();    //开始播放
+
+
+        }
+
+        private void windowsUIButtonPanel_ButtonClick(object sender, DevExpress.XtraBars.Docking2010.ButtonEventArgs e)
+        {
+            WindowsUIButton btn = e.Button as WindowsUIButton;
+            string tag = btn.Tag as string;
+            switch (tag)
+            {
+                case "pre":
+                    mediaPlayer.Ctlcontrols.previous();
+                    break;
+                case "next":
+                    mediaPlayer.Ctlcontrols.next();
+                    break;
+                case "pause":
+                    mediaPlayer.Ctlcontrols.pause();
+                    btn.ImageOptions.Image = Resources.play_32x32;
+                    btn.Tag = "start";
+                    break;
+                case "start":
+                    mediaPlayer.Ctlcontrols.play();
+                    btn.ImageOptions.Image = Resources.pause_32x32;
+                    btn.Tag = "pause";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
